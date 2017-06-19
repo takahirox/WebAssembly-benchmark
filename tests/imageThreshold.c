20f80b9c7b3bb07bbcda5650f913567f6bd440e5
Takahiro <hogehoge@gachapin.jp>
2017-06-19 15:52:45 +0900

    Update Threshold logic

--- tests/imageThreshold.c
+++ tests/imageThreshold.c
@@ -25,18 +25,22 @@ void imageThreshold(unsigned char *data, int width, int height) {
     for (int j = 0; j < height; j++) {
       int x1 = i - s2;
       int x2 = i + s2;
+      int x1_1 = x1 - 1;
       int y1 = j - s2;
       int y2 = j + s2;
+      int y1_1 = y1 - 1;
       if (x1 < 0) x1 = 0;
       if (x2 >= width) x2 = width - 1;
+      if (x1_1 < 0) x1_1 = 0;
       if (y1 < 0) y1 = 0;
       if (y2 >= height) y2 = height - 1;
+      if (y1_1 < 0) y1_1 = 0;
       int count = (x2 - x1) * (y2 - y1);
       int index = j * width + i;
       int index1 = y2 * width + x2;
-      int index2 = y1 * width + x2;
-      int index3 = y2 * width + x1;
-      int index4 = y1 * width + x1;
+      int index2 = y1_1 * width + x2;
+      int index3 = y2 * width + x1_1;
+      int index4 = y1_1 * width + x1_1;
       int sum = array[index1] - array[index2] - array[index3] + array[index4];
       if (data[index * 4] * count <= sum * t2) {
         data[index * 4 + 0] = data[index * 4 + 1] = data[index * 4 + 2] = 0;
