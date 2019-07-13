8376ef53c9ba47ae132c2c4ca71fefc1e7ac74c1
mavrukin <mavrukin@gmail.com>
2019-07-13 22:46:34 +0300

    make same fixes for shared buffer / cleanup for sumDouble as for sumInt.  After running with the updated changes, the new results bring sumDouble to parity w/ JS as to before it was at best 70% as performant

--- tests/sumDouble.sh
+++ tests/sumDouble.sh
@@ -1,9 +1,5 @@
 emcc sumDouble.c \
   -O3 \
-  -s WASM=1 \
-  -s MODULARIZE=1 \
-  -s DEMANGLE_SUPPORT=1 \
   -s ALLOW_MEMORY_GROWTH=1 \
-  -s "EXPORTED_FUNCTIONS=['_sumDouble', '_malloc', '_free']" \
-  -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' \
-  -o sumDouble.js
+  -s "EXPORTED_FUNCTIONS=['_sumDouble']" \
+  -o sumDouble.wasm
