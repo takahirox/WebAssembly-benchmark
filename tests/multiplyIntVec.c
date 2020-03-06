1d3d72d5b379775c0fb6ad375b12df4ce8704934
Michael Avrukin <mavrukin@gmail.com>
2020-03-06 16:11:56 -0500

    fix up and update multiplyIntVec for latest emcc

--- tests/multiplyIntVec.c
+++ tests/multiplyIntVec.c
@@ -1,5 +1,6 @@
 void multiplyIntVec(const int* src1, const int* src2, int* res, const int n) {
-  for (int i = 0; i < n; i++) {
+  int i = 0;
+  for (; i < n; i++) {
     res[i] = src1[i] * src2[i];
   }
 }
