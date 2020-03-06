49424742a19b84f41383d4c3f34de488f2d3cad0
Michael Avrukin <mavrukin@gmail.com>
2020-03-05 21:25:27 -0500

    small cleanup of the sumInt c version

--- tests/sumInt.c
+++ tests/sumInt.c
@@ -1,7 +1,8 @@
 int sumInt(const int* array, const int n) {
   int s = 0;
-  for (int i = 0; i < n; i++) {
-    s += *(array + i);
+  int i = 0;
+  for (; i < n; i++) {
+    s += array[i];
   }
   return s;
 }
