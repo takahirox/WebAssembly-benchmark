774ba67ff8a231707d9880d74cb06de26597144c
Michael Avrukin <mavrukin@gmail.com>
2020-03-06 16:20:03 -0500

    fix up the multiplyDoubleVec for the new emcc

--- tests/multiplyDoubleVec.c
+++ tests/multiplyDoubleVec.c
@@ -1,5 +1,6 @@
 void multiplyDoubleVec(const double* src1, const double* src2, double *res, const int n) {
-  for (int i = 0; i < n; i++) {
+  int i = 0;
+  for (; i < n; i++) {
     res[i] = src1[i] * src2[i];
   }
 }
