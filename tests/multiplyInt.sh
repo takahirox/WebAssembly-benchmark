51fad8fdbce2c47dd81beb54c214d8214efa2d11
Michael Avrukin <mavrukin@gmail.com>
2020-03-09 20:49:33 -0400

    fix multiplyInt to use the emcc commands with better compilation, fix the loading in the HTML so that is uses streaming, and also write the optimized js multiplication per mt's example

--- tests/multiplyInt.sh
+++ tests/multiplyInt.sh
@@ -1,8 +1,7 @@
 emcc multiplyInt.c \
   -O3 \
-  -s WASM=1 \
-  -s MODULARIZE=1 \
-  -s DEMANGLE_SUPPORT=1 \
+  -ffast-math \
+  -s ALLOW_MEMORY_GROWTH=1 \
   -s "EXPORTED_FUNCTIONS=['_multiplyInt']" \
-  -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' \
-  -o multiplyInt.js
+  -o multiplyInt.wasm
+
